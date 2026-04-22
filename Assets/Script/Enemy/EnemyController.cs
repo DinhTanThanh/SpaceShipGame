59077c781efe552c83d563ca4427a0210282d7ca
DinhTanThanh <dinhtanthanh19@gmail.com>
2026-04-22 23:12:29 +0700

    change target player of ènemy

--- Assets/Script/Enemy/EnemyController.cs
+++ Assets/Script/Enemy/EnemyController.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class EnemyController : ShottingController
 {
+    [SerializeField] protected SpawnEnemy spawnEnemy;
+    public SpawnEnemy SpawnEnemy =>spawnEnemy;
+    protected override void Reset()
+    {
+        LoadComponentEnable();
+        LoadMeteoriteSO();
+    }
+    protected override void LoadComponentEnable()
+    {
+        this.spawnEnemy = FindAnyObjectByType<SpawnEnemy>();
+    }
     public override void LoadMeteoriteSO()
     {
         if (ShottingSO != null) return;
